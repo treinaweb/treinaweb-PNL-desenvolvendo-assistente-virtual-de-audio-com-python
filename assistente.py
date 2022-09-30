ef0878a75266e43e9a740ae7d2e09f5f444acbdd
Rodrigo <rcbm539@gmail.com>
2022-09-30 17:02:16 -0300

    02:01 - Definição de Horas

--- assistente.py
+++ assistente.py
@@ -2,35 +2,41 @@ from gtts import gTTS
 from playsound import playsound
 import speech_recognition as sr
 import os
+from datetime import datetime
 
-
-def cria_audio(audio,mensagem):
-	tts = gTTS(mensagem, lang="pt-br")
-	tts.save(audio)
-	playsound(audio)
-	os.remove(audio)
+def cria_audio(audio, mensagem):
+    tts = gTTS(mensagem, lang="pt-br")
+    tts.save(audio)
+    playsound(audio)
+    os.remove(audio)
 
 def monitora_audio():
-	recon = sr.Recognizer()
-	with sr.Microphone() as source:
-		while True:
-			print("Diga algo")
-			audio = recon.listen(source)
-			try:
-				mensagem = recon.recognize_google(audio, language='pt-br')
-				mensagem = mensagem.lower()
-				print("Você disse", mensagem)
-				cria_audio(mensagem)
-				break
-			except sr.UnknownValueError:
-				pass
-			except sr.RequestError:
-				pass
-		return mensagem
+    recon = sr.Recognizer()
+    with sr.Microphone() as source:
+        while True:
+            print("Diga alguma coisa")
+            audio = recon.listen(source)
+            try:
+                mensagem = recon.recognize_google(audio, language="pt-br")
+                mensagem = mensagem.lower()
+                print("Você disse ", mensagem)
+                executa_comandos(mensagem)
+                break
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError:
+                pass
+        return mensagem
 
 
+def executa_comandos(acao):
+    if 'horas' in acao:
+        hora = datetime.now().strftime("%H:%M")
+        frase = f"Agora são{hora}"
+        cria_audio("audio/mensagem.mp3", frase)
+   
 def main():
-	while True:
-		monitora_audio()
+    while True:
+        monitora_audio()
 
 main()
